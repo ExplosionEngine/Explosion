fc7b8a938d8e8d6924c5b75649dbb36fed24010e
John Kindem <461425614@qq.com>
2020-07-14 00:36:29 +0800

    update demo

--- example/macos/main.cpp
+++ example/macos/main.cpp
@@ -1,70 +1,21 @@
 #include <iostream>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-#define WINDOW_TITLE "gl-playground"
-
-void init();
-void draw_frame();
-void on_input(GLFWwindow* window);
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+#include <Render/Renderer.h>
+using namespace std;
+using namespace Explosion;
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
-    if (window == nullptr) {
-        std::cout << "failed to create GLFW window" << std::endl;
-        glfwTerminate();
+    Renderer* renderer = Renderer::Create(RendererType::OpenGL);
+    if (!renderer->Init(800, 600, "macos-example")) {
+        cout << "failed to init renderer" << endl;
+        Renderer::Destroy(renderer);
         return -1;
     }
-    glfwMakeContextCurrent(window);
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
-        std::cout << "failed to initialize GLAD" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    init();
-    while (!glfwWindowShouldClose(window)) {
-        on_input(window);
-        draw_frame();
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+    while (renderer->IsValid()) {
+        renderer->BeginFrame();
+        renderer->EndFrame();
     }
 
-    glfwTerminate();
     return 0;
-}
-
-void init()
-{
-
-}
-
-void draw_frame()
-{
-    glClearColor(.2f, .3f, .3f, 1.f);
-    glClear(GL_COLOR_BUFFER_BIT);
-}
-
-void on_input(GLFWwindow* window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, true);
-    }
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    glViewport(0, 0, width, height);
 }
--- include/Render/Renderer.h
+++ include/Render/Renderer.h
@@ -1,8 +1,8 @@
 #ifndef EXPLOSION_RENDERER_H
 #define EXPLOSION_RENDERER_H
 
-#include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <Common/RendererType.h>
 
 namespace Explosion {
