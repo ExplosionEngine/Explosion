a144066c3d00a61318aaf5e4e399b33dd40478a6
John Kindem <461425614@qq.com>
2020-07-15 22:28:53 +0800

    complete shader

--- include/Render/Shader/Shader.h
+++ include/Render/Shader/Shader.h
@@ -3,23 +3,29 @@
 
 #include <glad/glad.h>
 
+static const int INFO_BUFFER_SIZE = 1024;
+
 namespace Explosion {
 class Shader {
 private:
-    Shader() = default;
+    Shader();
     ~Shader();
+
+    void DeleteVertex();
+    void DeleteFragment();
+    void DeleteProgram();
 
     GLuint programId = GL_NONE;
     GLuint vertexShaderId = GL_NONE;
     GLuint fragmentShaderId = GL_NONE;
-    char info[1024] {};
+    char info[INFO_BUFFER_SIZE] {};
 protected:
 public:
     static Shader* Create();
     static void Destroy(Shader* shader);
 
-    bool CompileVertex(const char *file);
-    bool CompileFragment(const char * file);
+    bool CompileVertex(const char* source);
+    bool CompileFragment(const char* source);
     bool Link();
     char* GetInfo();
 };
