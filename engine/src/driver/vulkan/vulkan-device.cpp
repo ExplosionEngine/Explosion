3176dec864f47425d03b98fcaa04d7d9b6e21f83
John Kindem <461425614@qq.com>
2021-01-09 15:28:24 +0800

    update code for debug utils

--- engine/src/driver/vulkan/vulkan-device.cpp
+++ engine/src/driver/vulkan/vulkan-device.cpp
@@ -5,7 +5,7 @@
 #include <driver/vulkan/vulkan-device.h>
 
 namespace {
-    static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
+    VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
         VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverityFlagBits,
         VkDebugUtilsMessageTypeFlagsEXT messageTypeFlags,
         const VkDebugUtilsMessengerCallbackDataEXT* callbackData,
@@ -19,10 +19,10 @@ namespace {
         VkDebugUtilsMessengerCreateInfoEXT createInfo {};
         createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
         createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
-                                     | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+             | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
         createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
-                                 | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
-                                 | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+             | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
+             | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
         createInfo.pfnUserCallback = DebugCallback;
         createInfo.pUserData = nullptr;
         return createInfo;
