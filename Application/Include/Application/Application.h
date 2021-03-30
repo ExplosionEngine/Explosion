644b4a75b7234ff5e5a5dfada3c8fef774f63e9d
John Kindem <461425614@qq.com>
2021-03-31 00:33:22 +0800

    update code for application module

--- Application/Include/Application/Application.h
+++ Application/Include/Application/Application.h
@@ -5,11 +5,34 @@
 #ifndef EXPLOSION_APPLICATION_H
 #define EXPLOSION_APPLICATION_H
 
+#include <cstdint>
+#include <string>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 namespace Explosion {
     class Application {
     public:
+        Application(void* window, std::string name, uint32_t width, uint32_t height);
+        ~Application();
+        void Run();
+
+    protected:
+        void* GetWindow() const;
+        uint32_t GetWidth() const;
+        uint32_t GetHeight() const;
+
+        virtual void OnStart() = 0;
+        virtual void OnStop() = 0;
+        virtual void OnDrawFrame() = 0;
 
     private:
+        void* window;
+        std::string name;
+        uint32_t width;
+        uint32_t height;
+        GLFWwindow* glfwWindow;
     };
 }
 
--- Application/Src/Application.cpp
+++ Application/Src/Application.cpp
@@ -2,3 +2,33 @@
 // Created by John Kindem on 2021/3/30.
 //
 
+#include <Application/Application.h>
+
+#include <utility>
+
+namespace Explosion {
+    Application::Application(void* window, std::string name, uint32_t width, uint32_t height)
+        : window(window), name(std::move(name)), width(width), height(height), glfwWindow(nullptr) {}
+
+    Application::~Application() = default;
+
+    void Application::Run()
+    {
+        // TODO
+    }
+
+    void* Application::GetWindow() const
+    {
+        return window;
+    }
+
+    uint32_t Application::GetWidth() const
+    {
+        return width;
+    }
+
+    uint32_t Application::GetHeight() const
+    {
+        return height;
+    }
+}
