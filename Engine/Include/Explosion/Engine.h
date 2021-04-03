d06de7637ba9fba1d2d2987ff37b9eb2e050b13c
John Kindem <461425614@qq.com>
2021-04-03 16:36:16 +0800

    update CMakeLists

---
+++ Engine/Include/Explosion/Engine.h
@@ -0,0 +1,19 @@
+//
+// Created by John Kindem on 2021/3/23.
+//
+
+#ifndef EXPLOSION_ENGINE_H
+#define EXPLOSION_ENGINE_H
+
+#include <c++/cstdint>
+#include <c++/string>
+
+namespace Explosion {
+    class Engine {
+    public:
+
+    private:
+    };
+}
+
+#endif //EXPLOSION_ENGINE_H
---
+++ Engine/Src/Engine.cpp
@@ -0,0 +1,9 @@
+//
+// Created by John Kindem on 2021/3/23.
+//
+
+#include <Explosion/Engine.h>
+
+namespace Explosion {
+
+}
