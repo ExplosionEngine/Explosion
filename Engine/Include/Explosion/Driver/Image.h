08c5cb95807a2480194cead38d7591532c6f328a
John Kindem <461425614@qq.com>
2021-04-13 23:47:20 +0800

    update code for image

--- Engine/Include/Explosion/Driver/Image.h
+++ Engine/Include/Explosion/Driver/Image.h
@@ -8,14 +8,96 @@
 #include <vulkan/vulkan.h>
 
 namespace Explosion {
+    class SwapChain;
+    class Device;
+
     class Image {
     public:
-        explicit Image(const VkImage& vkImage);
-        ~Image();
+        struct Info {};
+
+        virtual ~Image() = 0;
+        virtual const VkImage& GetVkImage() = 0;
+        virtual const VkImageView& GetVkImageView() = 0;
+        virtual const Info& GetInfo() = 0;
+
+    protected:
+        Image();
+    };
+
+    class SwapChainImage : public Image {
+    public:
+        SwapChainImage(Device& device, SwapChain& swapChain, uint32_t imageIndex);
+        ~SwapChainImage() override;
+        VkImage const& GetVkImage() override;
+        VkImageView const& GetVkImageView() override;
+        const Info& GetInfo() override;
 
     private:
-        bool holder = false;
+        void FetchImage();
+        void CreateImageView();
+        void DestroyImageView();
+
+        void WriteImageInfo();
+
+        Device& device;
+        SwapChain& swapChain;
+        Info info;
+        uint32_t imageIndex;
         VkImage vkImage = VK_NULL_HANDLE;
+        VkImageView vkImageView = VK_NULL_HANDLE;
+    };
+
+    class Attachment : public Image {
+    public:
+        struct Config {};
+
+        Attachment(Device& device, Config config);
+        ~Attachment() override;
+        VkImage const& GetVkImage() override;
+        VkImageView const& GetVkImageView() override;
+        const Info& GetInfo() override;
+
+    private:
+        void CreateImage();
+        void DestroyImage();
+
+        void CreateImageView();
+        void DestroyImageView();
+
+        void WriteImageInfo();
+
+        Device& device;
+        Config config;
+        Info info;
+        VkImage vkImage = VK_NULL_HANDLE;
+        VkImageView vkImageView = VK_NULL_HANDLE;
+    };
+
+    class ExternalImage : public Image {
+    public:
+        struct Config {};
+
+        ExternalImage(Device& device, Config config);
+        ~ExternalImage() override;
+        void Update(void* data);
+        VkImage const& GetVkImage() override;
+        VkImageView const& GetVkImageView() override;
+        const Info& GetInfo() override;
+
+    private:
+        void CreateImage();
+        void DestroyImage();
+
+        void CreateImageView();
+        void DestroyImageView();
+
+        void WriteImageInfo();
+
+        Device& device;
+        Config config;
+        Info info;
+        VkImage vkImage = VK_NULL_HANDLE;
+        VkImageView vkImageView = VK_NULL_HANDLE;
     };
 }
 
--- Engine/Src/Driver/Image.cpp
+++ Engine/Src/Driver/Image.cpp
@@ -5,7 +5,166 @@
 #include <Explosion/Driver/Image.h>
 
 namespace Explosion {
-    Image::Image(VkImage const& vkImage) : holder(false), vkImage(vkImage) {}
+    Image::Image() = default;
 
-    Image::~Image() {}
+    SwapChainImage::SwapChainImage(Device& device, SwapChain& swapChain, uint32_t imageIndex)
+        : device(device), swapChain(swapChain), imageIndex(imageIndex)
+    {
+        FetchImage();
+        CreateImageView();
+        WriteImageInfo();
+    }
+
+    SwapChainImage::~SwapChainImage()
+    {
+        DestroyImageView();
+    }
+
+    VkImage const& SwapChainImage::GetVkImage()
+    {
+        return vkImage;
+    }
+
+    VkImageView const& SwapChainImage::GetVkImageView()
+    {
+        return vkImageView;
+    }
+
+    const Image::Info& SwapChainImage::GetInfo()
+    {
+        return info;
+    }
+
+    void SwapChainImage::FetchImage()
+    {
+        // TODO
+    }
+
+    void SwapChainImage::CreateImageView()
+    {
+        // TODO
+    }
+
+    void SwapChainImage::DestroyImageView()
+    {
+        // TODO
+    }
+
+    void SwapChainImage::WriteImageInfo()
+    {
+        // TODO
+    }
+
+    Attachment::Attachment(Device& device, Attachment::Config config)
+        : device(device), config(config)
+    {
+        CreateImage();
+        CreateImageView();
+        WriteImageInfo();
+    }
+
+    Attachment::~Attachment()
+    {
+        DestroyImageView();
+        DestroyImage();
+    }
+
+    VkImage const& Attachment::GetVkImage()
+    {
+        return vkImage;
+    }
+
+    VkImageView const& Attachment::GetVkImageView()
+    {
+        return vkImageView;
+    }
+
+    const Image::Info& Attachment::GetInfo()
+    {
+        return info;
+    }
+
+    void Attachment::CreateImage()
+    {
+        // TODO
+    }
+
+    void Attachment::DestroyImage()
+    {
+        // TODO
+    }
+
+    void Attachment::CreateImageView()
+    {
+        // TODO
+    }
+
+    void Attachment::DestroyImageView()
+    {
+        // TODO
+    }
+
+    void Attachment::WriteImageInfo()
+    {
+        // TODO
+    }
+
+    ExternalImage::ExternalImage(Device& device, ExternalImage::Config config)
+        : device(device), config(config)
+    {
+        CreateImage();
+        CreateImageView();
+        WriteImageInfo();
+    }
+
+    ExternalImage::~ExternalImage()
+    {
+        DestroyImageView();
+        DestroyImage();
+    }
+
+    void ExternalImage::Update(void* data)
+    {
+        // TODO
+    }
+
+    VkImage const& ExternalImage::GetVkImage()
+    {
+        return vkImage;
+    }
+
+    VkImageView const& ExternalImage::GetVkImageView()
+    {
+        return vkImageView;
+    }
+
+    const Image::Info& ExternalImage::GetInfo()
+    {
+        return info;
+    }
+
+    void ExternalImage::CreateImage()
+    {
+        // TODO
+    }
+
+    void ExternalImage::DestroyImage()
+    {
+        // TODO
+    }
+
+    void ExternalImage::CreateImageView()
+    {
+        // TODO
+    }
+
+    void ExternalImage::DestroyImageView()
+    {
+        // TODO
+    }
+
+    void ExternalImage::WriteImageInfo()
+    {
+        // TODO
+    }
 }
