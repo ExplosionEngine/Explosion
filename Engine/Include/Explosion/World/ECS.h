7e780523f5aebc0b6be26cebd477982d1b800ef4
John Kindem <461425614@qq.com>
2021-06-06 14:36:46 +0800

    ECS basic

--- Engine/Include/Explosion/World/ECS.h
+++ Engine/Include/Explosion/World/ECS.h
@@ -14,26 +14,6 @@ namespace Explosion::ECS {
 
     struct Component {};
 
-    template <typename... Args>
-    class View {
-    public:
-        explicit View(entt::view<Args...>& view) : view(view) {}
-        ~View() = default;
-
-        void Each(const std::function<void(const Args&...)>& func)
-        {
-            view.template each(func);
-        }
-
-        void Each(const std::function<void(Entity entity, const Args&...)>& func)
-        {
-            view.template each(func);
-        }
-
-    private:
-        entt::view<Args...>& view;
-    };
-
     class Registry {
     public:
         Registry() = default;
@@ -50,21 +30,15 @@ namespace Explosion::ECS {
         }
 
         template <typename Comp, typename... Args>
-        void AddComponent(Entity entity, const Args&... args)
+        void AddComponent(Entity entity, Args&&... args)
         {
             registry.template emplace_or_replace<Comp>(entity, args...);
         }
 
         template <typename Comp>
-        Comp& GetComponent(Entity entity)
+        Comp* GetComponent(Entity entity)
         {
-            registry.template get<Comp>(entity);
-        }
-
-        template <typename... Args>
-        View<Args...> View()
-        {
-            return registry.template view<Args...>();
+            return registry.template try_get<Comp>(entity);
         }
 
     private:
