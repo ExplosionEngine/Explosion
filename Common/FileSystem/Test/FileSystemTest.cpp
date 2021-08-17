42b613fda70d91ea0358e3509895ba986a25181e
Reve4Mevol <2664806516@qq.com>
2021-08-17 22:15:49 +0800

    fix : 解决 issue #126 , 完善 UT .

--- Common/FileSystem/Test/FileSystemTest.cpp
+++ Common/FileSystem/Test/FileSystemTest.cpp
@@ -10,9 +10,26 @@
 
 using namespace Explosion::FileSystem;
 
+const std::string testCaseDir = "./FileSystemTestCase";
+
+void remakeDir()
+{
+    fs::path path(testCaseDir);
+    fs::remove_all(path);
+    fs::create_directories(path);
+}
+
+void removeDir()
+{
+    fs::path path(testCaseDir);
+    fs::remove_all(path);
+}
+
+
 TEST(FileSystemTest, StringReadWriteTest)
 {
-    File tmpFile("./FSTestPath/TestFileSystem.txt");
+    remakeDir();
+    File tmpFile(testCaseDir + "/" + "StringReadWriteTest.txt");
     WriteStream wStream(tmpFile);
     std::string inputStr = "TestFileSystem";
     wStream.Open(FileType::TEXT);
@@ -28,11 +45,13 @@ TEST(FileSystemTest, StringReadWriteTest)
     rStream.Close();
 
     ASSERT_STREQ(inputStr.c_str(),outputStr.c_str());
+    removeDir();
 }
 
 TEST(FileSystemTest, NumberReadWriteTest)
 {
-    File tmpFile("./FSTestPath/TestFileSystem.txt");
+    remakeDir();
+    File tmpFile(testCaseDir + "/" + "NumberReadWriteTest.txt");
     WriteStream wStream(tmpFile);
     int32_t inputNum = 14356712;
     wStream.Open(FileType::TEXT);
@@ -48,22 +67,70 @@ TEST(FileSystemTest, NumberReadWriteTest)
     rStream.Close();
 
     ASSERT_EQ(inputNum,outputNum);
+    removeDir();
 }
 
 TEST(FileSystemTest, FileInterfaceTest)
 {
+    remakeDir();
     std::string filename = "FileTestCase";
     std::string extension = ".md";
     std::string fullname = filename + extension;
-    File tmpFile("./FSTestPath/" + fullname);
-    ASSERT_FALSE(tmpFile.IsExists());
+    File tmpFile(testCaseDir + "/" + fullname);
+    if (!tmpFile.IsExists()) {
+        tmpFile.Make();
+    }
 
-    tmpFile.Make();
     ASSERT_TRUE(tmpFile.IsExists());
-
     ASSERT_STREQ(tmpFile.GetFullName().c_str(),fullname.c_str());
     ASSERT_STREQ(tmpFile.GetExtension().c_str(),extension.c_str());
     ASSERT_STREQ(tmpFile.GetName().c_str(),filename.c_str());
     ASSERT_STREQ(tmpFile.GetRelativePath(tmpFile.GetParent()).c_str(),fullname.c_str());
+    removeDir();
+}
 
+TEST(FileSystemTest, DirectoryInterfaceTest)
+{
+    remakeDir();
+    std::vector<File> fileVec;
+    std::vector<Directory> dirVec;
+    for (int i = 0; i < 10; ++i) {
+        std::string fileName = testCaseDir + "/testFile" + std::to_string(i);
+        File tmpFile(fileName);
+        tmpFile.Make();
+        ASSERT_TRUE(tmpFile.IsExists());
+        fileVec.push_back(tmpFile);
+    }
+    for (int i = 0; i < 10; ++i) {
+        std::string dirName = testCaseDir + "/testDir" + std::to_string(i);
+        Directory tmpDir(dirName);
+        tmpDir.Make();
+        ASSERT_TRUE(tmpDir.IsExists());
+        dirVec.push_back(tmpDir);
+    }
+
+    Directory testDir(testCaseDir);
+    std::vector<File> fileListResult = testDir.ListFile();
+    std::vector<Directory> dirListResult = testDir.ListDir();
+    for (int i = 0; i < fileVec.size(); i++) {
+        bool sameFlag = false;
+        for (int j = 0; j < fileListResult.size(); j++) {
+            if (fileVec.at(i).GetAbsolutePath() == fileListResult[j].GetAbsolutePath()) {
+                sameFlag = true;
+                break;
+            }
+        }
+        EXPECT_TRUE(sameFlag) << fileVec.at(i).GetAbsolutePath() << " not exist in vector 'fileListResult'!" ;
+    }
+    for (int i = 0; i < dirVec.size(); i++) {
+        bool sameFlag = false;
+        for (int j = 0; j < dirListResult.size(); j++) {
+            if (dirVec.at(i).GetAbsolutePath() == dirListResult[j].GetAbsolutePath()) {
+                sameFlag = true;
+                break;
+            }
+        }
+        EXPECT_TRUE(sameFlag) << dirVec.at(i).GetAbsolutePath() << " not exist in vector 'fileListResult'!" ;
+    }
+    removeDir();
 }
