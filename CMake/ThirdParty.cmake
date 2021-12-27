e80db1ca82f4ba82a7654822a72136ba518b0c42
John Kindem <461425614@qq.com>
2021-12-28 00:22:13 +0800

    feat: add build option for 3rd package (most for header-only libs)

--- CMake/ThirdParty.cmake
+++ CMake/ThirdParty.cmake
@@ -1,8 +1,8 @@
-set(3RD_DIR "${CMAKE_SOURCE_DIR}/ThirdParty" CACHE PATH "" FORCE)
-set(3RD_ZIP_DIR "${3RD_DIR}/Zip" CACHE PATH "" FORCE)
-set(3RD_SOURCE_DIR "${3RD_DIR}/Lib" CACHE PATH "" FORCE)
-set(3RD_BINARY_DIR "${CMAKE_BINARY_DIR}/ThirdPartyBuild" CACHE PATH "" FORCE)
-set(3RD_INSTALL_DIR "${CMAKE_BINARY_DIR}/ThirdPartyInstall" CACHE PATH "" FORCE)
+set(3RD_DIR ${CMAKE_SOURCE_DIR}/ThirdParty CACHE PATH "" FORCE)
+set(3RD_ZIP_DIR ${3RD_DIR}/Zip CACHE PATH "" FORCE)
+set(3RD_SOURCE_DIR ${3RD_DIR}/Lib CACHE PATH "" FORCE)
+set(3RD_BINARY_DIR ${CMAKE_BINARY_DIR}/ThirdPartyBuild CACHE PATH "" FORCE)
+set(3RD_INSTALL_DIR ${CMAKE_BINARY_DIR}/ThirdPartyInstall CACHE PATH "" FORCE)
 
 # ExtractZip
 # Description: extract zip file
@@ -39,24 +39,34 @@ endfunction()
 #  - VERSION {Single} : version of third party package
 #  - ARG     {List}   : arguments of cmake command
 function(AddThirdPartyPackage)
-    cmake_parse_arguments(PARAMS "" "NAME;VERSION" "ARG" ${ARGN})
+    cmake_parse_arguments(PARAMS "BUILD" "NAME;VERSION" "ARG" ${ARGN})
 
     set(3RD_PACKAGE_NAME "${PARAMS_NAME}")
     set(3RD_PACKAGE_FULL_NAME "${PARAMS_NAME}-${PARAMS_VERSION}")
     set(3RD_PACKAGE_SOURCE_DIR "${3RD_SOURCE_DIR}/${3RD_PACKAGE_FULL_NAME}")
-    set(3RD_PACKAGE_BINARY_DIR "${3RD_BINARY_DIR}/${3RD_PACKAGE_NAME}")
-    set(3RD_PACKAGE_INSTALL_DIR "${3RD_INSTALL_DIR}/${3RD_PACKAGE_NAME}")
+    if (${PARAMS_BUILD})
+        set(3RD_PACKAGE_BINARY_DIR "${3RD_BINARY_DIR}/${3RD_PACKAGE_NAME}")
+        set(3RD_PACKAGE_INSTALL_DIR "${3RD_INSTALL_DIR}/${3RD_PACKAGE_NAME}")
+    endif()
 
     ExtractZip(
         SRC ${3RD_ZIP_DIR}/${3RD_PACKAGE_FULL_NAME}.zip
         DST ${3RD_SOURCE_DIR}/${3RD_PACKAGE_FULL_NAME}
     )
-    ExternalProject_Add(
-        ${3RD_PACKAGE_NAME}
-        SOURCE_DIR ${3RD_PACKAGE_SOURCE_DIR}
-        BINARY_DIR ${3RD_PACKAGE_BINARY_DIR}
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${3RD_PACKAGE_INSTALL_DIR}/$<CONFIG> ${PARAMS_ARG}
-        BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config $<CONFIG>
-        INSTALL_COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR> --config $<CONFIG>
-    )
+    if (${PARAMS_BUILD})
+        ExternalProject_Add(
+            ${3RD_PACKAGE_NAME}
+            SOURCE_DIR ${3RD_PACKAGE_SOURCE_DIR}
+            BINARY_DIR ${3RD_PACKAGE_BINARY_DIR}
+            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${3RD_PACKAGE_INSTALL_DIR}/$<CONFIG> ${PARAMS_ARG}
+            BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config $<CONFIG>
+            INSTALL_COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR> --config $<CONFIG>
+        )
+    endif()
+
+    set(${3RD_PACKAGE_NAME}_SOURCE_DIR ${3RD_PACKAGE_SOURCE_DIR} CACHE PATH "" FORCE)
+    if (${PARAMS_BUILD})
+        set(${3RD_PACKAGE_NAME}_BINARY_DIR ${3RD_PACKAGE_BINARY_DIR} CACHE PATH "" FORCE)
+        set(${3RD_PACKAGE_NAME}_INSTALL_DIR ${3RD_PACKAGE_INSTALL_DIR} CACHE PATH "" FORCE)
+    endif()
 endfunction()
