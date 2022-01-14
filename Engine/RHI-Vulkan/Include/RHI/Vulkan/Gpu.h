6fdd2763d3bd96f731518255b1b5a726bc5f80de
K <johnkindem@qq.com>
2022-01-14 09:40:37 +0800

    fix: fix a issue about include guard

--- Engine/RHI-Vulkan/Include/RHI/Vulkan/Gpu.h
+++ Engine/RHI-Vulkan/Include/RHI/Vulkan/Gpu.h
@@ -2,8 +2,8 @@
 // Created by johnk on 12/1/2022.
 //
 
-#ifndef EXPLOSION_RHI_DX12_GRAPHICS_CARD_H
-#define EXPLOSION_RHI_DX12_GRAPHICS_CARD_H
+#ifndef EXPLOSION_RHI_VULKAN_GPU_H
+#define EXPLOSION_RHI_VULKAN_GPU_H
 
 #include <vulkan/vulkan.hpp>
 
