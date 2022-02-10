1cee303c5a1913a1c395f4056d2b364cd7435078
John Kindem <461425614@qq.com>
2022-02-10 22:49:48 +0800

    feat: do not use git lfs anymore, support auto download 3rd package from explosion 3rd repo server

--- CMake/ThirdParty.cmake
+++ CMake/ThirdParty.cmake
@@ -1,44 +1,26 @@
+option(ENABLE_3RD_DEBUG_INFO "3rd package debug" ON)
+option(CUSTOM_3RD_REPO "using custom 3rd repo" OFF)
+
+if (${CUSTOM_3RD_REPO})
+    set(3RD_REPO ${3RD_REPO_URL} CACHE STRING "" FORCE)
+else()
+    set(3RD_REPO "http://124.223.92.162" CACHE STRING "" FORCE)
+endif()
+
 set(3RD_DIR ${CMAKE_SOURCE_DIR}/ThirdParty CACHE PATH "" FORCE)
 set(3RD_ZIP_DIR ${3RD_DIR}/Zip CACHE PATH "" FORCE)
 set(3RD_SOURCE_DIR ${3RD_DIR}/Lib CACHE PATH "" FORCE)
 set(3RD_BINARY_DIR ${CMAKE_BINARY_DIR}/ThirdPartyBuild CACHE PATH "" FORCE)
 set(3RD_INSTALL_DIR ${CMAKE_BINARY_DIR}/ThirdPartyInstall CACHE PATH "" FORCE)
 
-# ExtractZip
-# Description: extract zip file
-# Params:
-#  - SRC {Single} : source zip file
-#  - DST {Single} : output dir
-function(ExtractZip)
-    cmake_parse_arguments(PARAMS "" "SRC;DST" "" ${ARGN})
-
-    if (NOT EXISTS ${PARAMS_SRC})
-        message(FATAL_ERROR "Failed to found zip file: ${PARAMS_SRC}")
-    endif()
-    if (EXISTS ${PARAMS_DST})
-        message("Found extracted third party package: ${PARAMS_DST}")
-        return()
-    endif()
-    message("Starting extract zip file ${PARAMS_SRC}")
-
-    if (${WIN32})
-        execute_process(
-            COMMAND powershell Expand-Archive -Path ${PARAMS_SRC} -DestinationPath ${PARAMS_DST}
-        )
-    else()
-        execute_process(
-            COMMAND unzip -n ${PARAMS_SRC} -d ${PARAMS_DST}
-        )
-    endif()
-endfunction()
-
 # AddThirdPartyPackage
 # Description: add a third party package
 # Params:
-#  - NAME    {Single} : name of third party package
-#  - VERSION {Single} : version of third party package
-#  - ARG     {List}   : arguments of cmake command
-#  - BUILD   {Bool}   : build package or not (just using sources)
+#  - NAME     {Single} : name of third party package
+#  - VERSION  {Single} : version of third party package
+#  - ARG      {List}   : arguments of cmake command
+#  - BUILD    {Bool}   : build package or not (just using sources)
+#  - PLATFORM {Bool}   : true if package is platform relative
 function(AddThirdPartyPackage)
     cmake_parse_arguments(PARAMS "BUILD;PLATFORM" "NAME;VERSION" "ARG" ${ARGN})
 
@@ -48,6 +30,7 @@ function(AddThirdPartyPackage)
     else()
         set(3RD_PACKAGE_FULL_NAME "${PARAMS_NAME}-${PARAMS_VERSION}")
     endif()
+    set(3RD_PACKAGE_URL "${3RD_REPO}/${3RD_PACKAGE_FULL_NAME}.zip")
     set(3RD_PACKAGE_SOURCE_DIR "${3RD_SOURCE_DIR}/${3RD_PACKAGE_FULL_NAME}")
 
     if (${PARAMS_BUILD})
@@ -55,18 +38,38 @@ function(AddThirdPartyPackage)
         set(3RD_PACKAGE_INSTALL_DIR "${3RD_INSTALL_DIR}/${3RD_PACKAGE_NAME}/$<CONFIG>")
     endif()
 
-    ExtractZip(
-        SRC ${3RD_ZIP_DIR}/${3RD_PACKAGE_FULL_NAME}.zip
-        DST ${3RD_SOURCE_DIR}/${3RD_PACKAGE_FULL_NAME}
-    )
+    if (${ENABLE_3RD_DEBUG_INFO})
+        message("")
+        message("[3rd Package]")
+        message(" - name: ${PARAMS_NAME}")
+        message(" - version: ${PARAMS_VERSION}")
+        message(" - platform: ${PARAMS_PLATFORM}")
+        message(" - full name: ${3RD_PACKAGE_FULL_NAME}")
+        message(" - url: ${3RD_PACKAGE_URL}")
+        message("")
+    endif()
+
     if (${PARAMS_BUILD})
         ExternalProject_Add(
             ${3RD_PACKAGE_NAME}
+            URL ${3RD_PACKAGE_URL}
+            DOWNLOAD_DIR ${3RD_ZIP_DIR}
             SOURCE_DIR ${3RD_PACKAGE_SOURCE_DIR}
             BINARY_DIR ${3RD_PACKAGE_BINARY_DIR}
             CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${3RD_PACKAGE_INSTALL_DIR} ${PARAMS_ARG}
             BUILD_COMMAND ${CMAKE_COMMAND} --build <BINARY_DIR> --config $<CONFIG>
             INSTALL_COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR> --config $<CONFIG>
+        )
+    else()
+        ExternalProject_Add(
+            ${3RD_PACKAGE_NAME}
+            URL ${3RD_PACKAGE_URL}
+            DOWNLOAD_DIR ${3RD_ZIP_DIR}
+            SOURCE_DIR ${3RD_PACKAGE_SOURCE_DIR}
+            BINARY_DIR ${3RD_PACKAGE_BINARY_DIR}
+            CMAKE_ARGS ""
+            BUILD_COMMAND ""
+            INSTALL_COMMAND ""
         )
     endif()
 
