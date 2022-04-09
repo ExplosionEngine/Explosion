964838c1d1991ae36274f1a5f535e5413b559a4b
John Kindem <461425614@qq.com>
2022-04-09 18:42:44 +0800

    feat: update debug lib

--- Engine/Common/Include/Common/Debug.h
+++ Engine/Common/Include/Common/Debug.h
@@ -4,18 +4,26 @@
 
 #pragma once
 
+#include <string>
+#include <cstdint>
 #if BUILD_CONFIG_DEBUG
 #include <cassert>
+#include <iostream>
 #include <debugbreak.h>
 #endif
+
+#define Assert(expression) Common::Debug::AssertImpl(expression, #expression, __FILE__, __LINE__)
 
 namespace Common {
     class Debug {
     public:
-        static void Assert(bool expression)
+        static void AssertImpl(bool target, const std::string& name, const std::string& file, uint32_t line)
         {
 #if BUILD_CONFIG_DEBUG
-            /* TODO platform output */
+            if (target) {
+                return;
+            }
+            std::cerr << "Assert failed: " << name << ", " << file << ", " << line << std::endl;
             debug_break();
 #endif
         }
