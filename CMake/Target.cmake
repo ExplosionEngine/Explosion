60c470d645aa4d5dc88abd5c3f7c668e45289572
John Kindem <461425614@qq.com>
2022-04-13 22:18:00 +0800

    feat: add dll copy interface

--- CMake/Target.cmake
+++ CMake/Target.cmake
@@ -11,8 +11,9 @@ set(API_HEADER_DIR ${CMAKE_BINARY_DIR}/Api CACHE PATH "" FORCE)
 #  - INC  {List}   : private include directories of target
 #  - LINK {List}   : private link directories of target
 #  - LIB: {List}   : private libraries of target
+#  - DLL: {List}   : dll to copy (windows only)
 function(AddExecutable)
-    cmake_parse_arguments(PARAMS "" "NAME" "SRC;INC;LINK;LIB" ${ARGN})
+    cmake_parse_arguments(PARAMS "" "NAME" "SRC;INC;LINK;LIB;DLL" ${ARGN})
 
     if (${ENABLE_TARGET_DEBUG_INFO})
         message("")
@@ -40,6 +41,14 @@ function(AddExecutable)
         ${PARAMS_NAME}
         ${PARAMS_LIB}
     )
+    if ((${CMAKE_SYSTEM_NAME} STREQUAL "Windows") AND (DEFINED PARAMS_DLL))
+        foreach(D ${PARAMS_DLL})
+            add_custom_command(
+                TARGET ${PARAMS_NAME} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different ${D} $<TARGET_FILE_DIR:${PARAMS_NAME}>
+            )
+        endforeach()
+    endif()
 endfunction()
 
 # AddLibrary
