3ef72f54c41af4d7653f1c38c1957827cb5e0477
John Kindem <461425614@qq.com>
2022-07-03 23:13:27 +0800

    feat: shader compiler base framework

--- Engine/Source/Shader/Include/Shader/ShaderCompiler.h
+++ Engine/Source/Shader/Include/Shader/ShaderCompiler.h
@@ -4,6 +4,29 @@
 
 #pragma once
 
+#include <Shader/Shader.h>
+
 namespace Shader {
-    // TODO
+    class ShaderCompiler {
+    public:
+        static ShaderCompiler& Get()
+        {
+            static ShaderCompiler instance;
+            return instance;
+        }
+
+        ~ShaderCompiler() = default;
+
+        template <typename T>
+        void CompileEngineShader()
+        {
+            // TODO using single thread to compile now, wille replace it with multi thread / multi process in the future
+            EngineShaderMap<T>::Get().TraverseVariant([](const auto& variantSet, auto& shader) -> void {
+                // TODO
+            });
+        }
+
+    private:
+        ShaderCompiler() = default;
+    };
 }
