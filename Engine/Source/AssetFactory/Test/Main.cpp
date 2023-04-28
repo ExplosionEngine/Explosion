608fd5670bbc938baa5a2616413c0c32c0cb0451
John Kindem <461425614@qq.com>
2023-04-28 18:09:53 +0800

    feat: import assimp lib

---
+++ Engine/Source/AssetFactory/Test/Main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by johnk on 2023/4/28.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
