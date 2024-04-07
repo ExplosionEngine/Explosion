60973f534112c70725f15e72463a3ca1c46f9fc4
John Kindem <461425614@qq.com>
2024-04-07 23:55:57 +0800

    feat: engine basic

--- Engine/Source/Runtime/Include/Runtime/RuntimeModule.h
+++ Engine/Source/Runtime/Include/Runtime/RuntimeModule.h
@@ -4,17 +4,31 @@
 
 #pragma once
 
+#include <Common/Memory.h>
 #include <Core/Module.h>
 #include <Runtime/Api.h>
 #include <Runtime/World.h>
+#include <Runtime/Engine.h>
 
 namespace Runtime {
+    struct RuntimeModuleInitParams {
+        bool isEditor;
+    };
+
     class RUNTIME_API RuntimeModule : public Core::Module {
     public:
         RuntimeModule();
         ~RuntimeModule() override;
 
+        void Initialize(const RuntimeModuleInitParams& inParams);
+        Engine* GetEngine();
         World* CreateWorld(const std::string& inName = "") const;
         void DestroyWorld(World* inWorld) const;
+
+    private:
+        void CreateEngine();
+
+        bool initialized;
+        Common::UniqueRef<Engine> engine;
     };
 }
