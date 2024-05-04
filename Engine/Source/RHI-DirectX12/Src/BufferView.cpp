2392dde57ff25f0b9b3b0367866e1a9501cb79f6
John Kindem <461425614@qq.com>
2024-05-04 19:17:06 +0800

    refactor: update dx12 rhi buffer view usage checks

--- Engine/Source/RHI-DirectX12/Src/BufferView.cpp
+++ Engine/Source/RHI-DirectX12/Src/BufferView.cpp
@@ -10,28 +10,6 @@
 #include <RHI/DirectX12/Common.h>
 
 namespace RHI::DirectX12 {
-    static inline bool IsConstantBuffer(BufferViewType type, BufferUsageFlags bufferUsages)
-    {
-        return type == BufferViewType::uniformBinding && (bufferUsages & BufferUsageBits::uniform) != 0;
-    }
-
-    static inline bool IsUnorderedAccessBuffer(BufferViewType type, BufferUsageFlags bufferUsages)
-    {
-        return type == BufferViewType::storageBinding && (bufferUsages & BufferUsageBits::storage) != 0;
-    }
-
-    static inline bool IsVertexBuffer(BufferViewType type, BufferUsageFlags bufferUsages)
-    {
-        return type == BufferViewType::vertex && (bufferUsages & BufferUsageBits::vertex) != 0;
-    }
-
-    static inline bool IsIndexBuffer(BufferViewType type, BufferUsageFlags bufferUsages)
-    {
-        return type == BufferViewType::index && (bufferUsages & BufferUsageBits::index) != 0;
-    }
-}
-
-namespace RHI::DirectX12 {
     DX12BufferView::DX12BufferView(DX12Buffer& inBuffer, const BufferViewCreateInfo& inCreateInfo)
         : BufferView(inCreateInfo), buffer(inBuffer)
     {
@@ -57,14 +35,20 @@ namespace RHI::DirectX12 {
 
     void DX12BufferView::CreateNativeView(const BufferViewCreateInfo& inCreateInfo)
     {
-        if (IsConstantBuffer(inCreateInfo.type, buffer.GetUsages())) {
+        const auto bufferUsages = buffer.GetUsages();
+
+        if (inCreateInfo.type == BufferViewType::uniformBinding) {
+            Assert((bufferUsages & BufferUsageBits::uniform) != 0);
+
             D3D12_CONSTANT_BUFFER_VIEW_DESC desc {};
             desc.BufferLocation = buffer.GetNative()->GetGPUVirtualAddress() + inCreateInfo.offset;
             desc.SizeInBytes = Common::AlignUp<D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT>(inCreateInfo.size);
 
             nativeView = buffer.GetDevice().AllocateCbvSrvUavDescriptor();
             buffer.GetDevice().GetNative()->CreateConstantBufferView(&desc, std::get<Common::UniqueRef<DescriptorAllocation>>(nativeView)->GetCpuHandle());
-        } else if (IsUnorderedAccessBuffer(inCreateInfo.type, buffer.GetUsages())) {
+        } else if (inCreateInfo.type == BufferViewType::storageBinding) {
+            Assert((bufferUsages & BufferUsageBits::storage) != 0);
+
             D3D12_UNORDERED_ACCESS_VIEW_DESC desc {};
             desc.Format = DXGI_FORMAT_UNKNOWN;
             desc.ViewDimension = D3D12_UAV_DIMENSION_BUFFER;
@@ -73,18 +57,24 @@ namespace RHI::DirectX12 {
 
             nativeView = buffer.GetDevice().AllocateCbvSrvUavDescriptor();
             buffer.GetDevice().GetNative()->CreateUnorderedAccessView(buffer.GetNative(), nullptr, &desc, std::get<Common::UniqueRef<DescriptorAllocation>>(nativeView)->GetCpuHandle());
-        } else if (IsVertexBuffer(inCreateInfo.type, buffer.GetUsages())) {
+        } else if (inCreateInfo.type == BufferViewType::vertex) {
+            Assert((bufferUsages & BufferUsageBits::vertex) != 0);
+
             nativeView = D3D12_VERTEX_BUFFER_VIEW();
             D3D12_VERTEX_BUFFER_VIEW& vertexBufferView = std::get<D3D12_VERTEX_BUFFER_VIEW>(nativeView);
             vertexBufferView.BufferLocation = buffer.GetNative()->GetGPUVirtualAddress() + inCreateInfo.offset;
             vertexBufferView.SizeInBytes = inCreateInfo.size;
             vertexBufferView.StrideInBytes = std::get<VertexBufferViewInfo>(inCreateInfo.extend).stride;
-        } else if (IsIndexBuffer(inCreateInfo.type, buffer.GetUsages())) {
+        } else if (inCreateInfo.type == BufferViewType::index) {
+            Assert((bufferUsages & BufferUsageBits::index) != 0);
+
             nativeView = D3D12_INDEX_BUFFER_VIEW();
             D3D12_INDEX_BUFFER_VIEW& indexBufferView = std::get<D3D12_INDEX_BUFFER_VIEW>(nativeView);
             indexBufferView.BufferLocation = buffer.GetNative()->GetGPUVirtualAddress() + inCreateInfo.offset;
             indexBufferView.SizeInBytes = inCreateInfo.size;
             indexBufferView.Format = EnumCast<IndexFormat, DXGI_FORMAT>(std::get<IndexBufferViewInfo>(inCreateInfo.extend).format);
+        } else {
+            Unimplement();
         }
     }
 }
