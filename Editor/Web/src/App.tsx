e81ab1aa41b6530729d13f4b3ecd7725b5a069e6
FlyAndNotDown <johnkindem@qq.com>
2025-08-10 13:44:41 +0800

    feat: project hub basics

--- Editor/Web/src/App.tsx
+++ Editor/Web/src/App.tsx
@@ -1,19 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-
-import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
+import ProjectHubPage from "@/pages/project-hub";
 
 function App() {
   return (
     <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
+      <Route element={<ProjectHubPage/>} path="/project-hub"/>
     </Routes>
   );
 }
