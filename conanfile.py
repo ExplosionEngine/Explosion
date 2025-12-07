77bce426d1cb0ea7595172ccfe5120a51acc1332
kindem <johnkindem@qq.com>
2025-12-07 15:00:19 +0800

    refactor: use conan to manage assimp lib

--- conanfile.py
+++ conanfile.py
@@ -7,10 +7,13 @@ class ExplosionConan(ConanFile):
     def requirements(self):
         self.requires("cpp-httplib/0.27.0")
         self.requires("glfw/3.4")
-        self.requires("stb/cci.20240531")
+        self.requires("stb/cci.20230920")
         self.requires("cityhash/1.0.1")
         self.requires("gtest/1.17.0")
         self.requires("taskflow/3.10.0")
+        self.requires("assimp/6.0.2", options={
+            "shared": True
+        })
         self.requires("qt/6.8.3", options={
             "shared": True,
             "gui": True,
