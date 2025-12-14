6fc09709e116fad491ddea028e5f1ff43f8825c0
kindem <johnkindem@qq.com>
2025-12-14 18:45:00 +0800

    feat: use conan to manage debugbreak lib

--- conanfile.py
+++ conanfile.py
@@ -22,3 +22,4 @@ class ExplosionConan(ConanFile):
         # private repo
         self.requires("libclang/21.1.7-exp")
         self.requires("qt/6.10.1-exp")
+        self.requires("debugbreak/1.0-exp")
